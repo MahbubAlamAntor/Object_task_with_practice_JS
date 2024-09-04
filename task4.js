cf42a8c6ca513d2b13d686f69cffac240eb2d2bc
Mahbub8340 <mdmahbubalam573125@gmail.com>
2024-09-04 17:18:55 +0600

    all task complete

---
+++ task4.js
@@ -0,0 +1,10 @@
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+const keysArry = Object.keys(student)
+const count = keysArry.length;
+console.log(count)
